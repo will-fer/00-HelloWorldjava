fe29b78844520c21034fb4bb846a6b89c419fe9c
wferrand <wferrand@350028R-F115X10.lise.kreizenn.bzh>
2026-09-25 10:52:59 +0200

    EXO 4

--- src/pkg00/hello/world/HelloWorld.java
+++ src/pkg00/hello/world/HelloWorld.java
@@ -4,6 +4,8 @@
  */
 package pkg00.hello.world;
 
+import java.util.Scanner;
+
 /**
  *
  * @author wferrand
@@ -13,9 +15,26 @@ public class HelloWorld {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         System.out.println("Affichage d'un message dans laz console");
         System.out.println("sur plusieur lignes");
-    }
+    }*/
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = 0;
+        int y = 0;
+        
+        System.out.print("combien vaut x ? : ");
+        x = sc.nextInt();
+        System.out.print("combien vaut y ? : ");
+        y = sc.nextInt();
+        System.out.println("x est egale a " + x + " et y a " + y);
+        System.out.println("on iverse x et y");
+        int z = x;
+        int a = y;
+        x = a;
+        y = z;
+        System.out.println("maintenant x est egale a " + x + " et y a " + y);
+}
     
 }
